1fd47ad2f76e3ae218871132e99c3584089802fa
Florian Frank <flori@ping.de>
2005-04-23 12:36:05 +0000

    * added some tests for pair distances * changed levenshtein tests to floating point results

--- amatch/tests/test_levenshtein.rb
+++ amatch/tests/test_levenshtein.rb
@@ -2,91 +2,111 @@ require 'test/unit'
 require 'amatch'
 
 class TC_Levenshtein < Test::Unit::TestCase
+  D = 0.000001
+
   def setup
     @matcher = Amatch.new('test')
   end
 
   def test_match
-    assert_equal 4,     @matcher.match('')
-    assert_equal 0,     @matcher.match('test')
-    assert_equal 0,     @matcher.match('test')
-    assert_equal 1,     @matcher.match('testa')
-    assert_equal 1,     @matcher.match('atest')
-    assert_equal 1,     @matcher.match('teast')
-    assert_equal 1,     @matcher.match('est')
-    assert_equal 1,     @matcher.match('tes')
-    assert_equal 1,     @matcher.match('tst')
-    assert_equal 1,     @matcher.match('best')
-    assert_equal 1,     @matcher.match('tost')
-    assert_equal 1,     @matcher.match('tesa')
-    assert_equal 3,     @matcher.match('taex')
-    assert_equal 1.0,   @matcher.matchr('')
-    assert_equal 0.25,  @matcher.matchr('tesa')
-    assert_equal 6,     @matcher.match('aaatestbbb')
+    assert_in_delta 4,     @matcher.match(''), D
+    assert_in_delta 0,     @matcher.match('test'), D
+    assert_in_delta 0,     @matcher.match('test'), D
+    assert_in_delta 1,     @matcher.match('testa'), D
+    assert_in_delta 1,     @matcher.match('atest'), D
+    assert_in_delta 1,     @matcher.match('teast'), D
+    assert_in_delta 1,     @matcher.match('est'), D
+    assert_in_delta 1,     @matcher.match('tes'), D
+    assert_in_delta 1,     @matcher.match('tst'), D
+    assert_in_delta 1,     @matcher.match('best'), D
+    assert_in_delta 1,     @matcher.match('tost'), D
+    assert_in_delta 1,     @matcher.match('tesa'), D
+    assert_in_delta 3,     @matcher.match('taex'), D
+    assert_in_delta 1,     @matcher.matchr(''), D
+    assert_in_delta 0.25,  @matcher.matchr('tesa'), D
+    assert_in_delta 6,     @matcher.match('aaatestbbb'), D
   end
 
   def test_search
-    assert_equal 4,     @matcher.search('')
-    assert_equal 1.0,   @matcher.searchr('')
-    assert_equal 0,     @matcher.search('aaatestbbb')
-    assert_equal 3,     @matcher.search('aaataexbbb')
-    assert_equal 0.75,  @matcher.searchr('aaataexbbb')
-    assert_equal 4,     @matcher.search('aaaaaaaaa')
-    assert_equal 1.0,   @matcher.searchr('aaaaaaaaa')
+    assert_in_delta 4,     @matcher.search(''), D
+    assert_in_delta 1,     @matcher.searchr(''), D
+    assert_in_delta 0,     @matcher.search('aaatestbbb'), D
+    assert_in_delta 3,     @matcher.search('aaataexbbb'), D
+    assert_in_delta 0.75,  @matcher.searchr('aaataexbbb'), D
+    assert_in_delta 4,     @matcher.search('aaaaaaaaa'), D
+    assert_in_delta 1,     @matcher.searchr('aaaaaaaaa'), D
   end
 
   def test_compare
-    assert_equal -4,    @matcher.compare('')
-    assert_equal -1.0,  @matcher.comparer('')
-    assert_equal 3,     @matcher.compare('taex')
-    assert_equal 0.25,  @matcher.comparer('tesa')
-    assert_equal 6,     @matcher.compare('aaatestbbb')
-    assert_equal 0,     @matcher.compare('test')
-    assert_equal -2,    @matcher.compare('tex')
-    assert_equal -0.5,  @matcher.comparer('tsa')
-    assert_equal 4,     @matcher.compare('wxyz')
-    assert_equal 1.0,   @matcher.comparer('wxyz')
+    assert_in_delta -4,    @matcher.compare(''), D
+    assert_in_delta -1,    @matcher.comparer(''), D
+    assert_in_delta 3,     @matcher.compare('taex'), D
+    assert_in_delta 0.25,  @matcher.comparer('tesa'), D
+    assert_in_delta 6,     @matcher.compare('aaatestbbb'), D
+    assert_in_delta 0,     @matcher.compare('test'), D
+    assert_in_delta -2,    @matcher.compare('tex'), D
+    assert_in_delta -0.5,  @matcher.comparer('tsa'), D
+    assert_in_delta 4,     @matcher.compare('wxyz'), D
+    assert_in_delta 1,     @matcher.comparer('wxyz'), D
     assert_raises(TypeError) { @matcher.match(:foo) }
   end
 
+  def assert_in_delta_array(left, right, delta = D)
+    left.size.times do |i|
+      assert_in_delta left[i], right[i], delta
+    end
+  end
+
   def test_array_result
-    assert_equal [2, 0],    @matcher.match(["tets", "test"]);
-    assert_equal [0.5, 0],  @matcher.matchr(["tets", "test"]);
-    assert_equal [2, 0],    @matcher.compare(["tets", "test"]);
-    assert_equal [0.5, 0],  @matcher.comparer(["tets", "test"]);
-    assert_equal [1, 0],    @matcher.search(["tetsaaa", "testaaa"]);
-    assert_equal [0.25, 0], @matcher.searchr(["tetsaaa", "testaaa"]);
+    assert_in_delta_array [2, 0],    @matcher.match(["tets", "test"])
+    assert_in_delta_array [0.5, 0],  @matcher.matchr(["tets", "test"])
+    assert_in_delta_array [2, 0],    @matcher.compare(["tets", "test"])
+    assert_in_delta_array [0.5, 0],  @matcher.comparer(["tets", "test"])
+    assert_in_delta_array [1, 0],    @matcher.search(["tetsaaa", "testaaa"])
+    assert_in_delta_array [0.25, 0], @matcher.searchr(["tetsaaa", "testaaa"])
     assert_raises(TypeError) { @matcher.match([:foo, "bar"]) }
   end
 
   def test_weights
-    assert_equal 1, @matcher.subw
-    assert_equal 1, @matcher.insw
-    assert_equal 1, @matcher.delw
+    assert_in_delta 1, @matcher.subw, D
+    assert_in_delta 1, @matcher.insw, D
+    assert_in_delta 1, @matcher.delw, D
     @matcher.subw = 2
-    assert_equal 2, @matcher.subw
-    assert_equal 2, @matcher.match('tast')
+    assert_in_delta 2, @matcher.subw, D
+    assert_in_delta 2, @matcher.match('tast'), D
     @matcher.subw = 1
-    assert_equal 1, @matcher.match('tast')
+    assert_in_delta 1, @matcher.match('tast'), D
     @matcher.insw = 2
-    assert_equal 2, @matcher.insw
-    assert_equal 2, @matcher.match('teist')
+    assert_in_delta 2, @matcher.insw, D
+    assert_in_delta 2, @matcher.match('teist'), D
     @matcher.delw = 2
-    assert_equal 2, @matcher.delw
-    assert_equal 2, @matcher.match('tst')
+    assert_in_delta 2, @matcher.delw, D
+    assert_in_delta 2, @matcher.match('tst'), D
     @matcher.resetw  
-    assert_equal 1, @matcher.subw
-    assert_equal 1, @matcher.insw
-    assert_equal 1, @matcher.delw
+    assert_in_delta 1, @matcher.subw, D
+    assert_in_delta 1, @matcher.insw, D
+    assert_in_delta 1, @matcher.delw, D
+    @matcher.subw = 0.5
+    assert_in_delta 0.5, @matcher.subw, D
+    assert_in_delta 0.5, @matcher.match('tast'), D
+    @matcher.subw = 1
+    assert_in_delta 1, @matcher.match('tast'), D
+    @matcher.insw = 0.5 
+    assert_in_delta 0.5, @matcher.insw, D
+    assert_in_delta 0.5, @matcher.match('teist'), D
+    @matcher.delw = 0.5
+    assert_in_delta 0.5, @matcher.delw, D
+    assert_in_delta 0.5, @matcher.match('tst'), D
+  end
+
+
+  def test_pattern_setting
     assert_raises(TypeError) { @matcher.pattern = :something }
     @matcher.pattern = 'test'
     assert_raises(TypeError) { @matcher.subw = :something }
-    @matcher.subw = 1
     assert_raises(TypeError) { @matcher.insw = :something }
-    @matcher.insw = 1
     assert_raises(TypeError) { @matcher.delw = :something }
-    @matcher.delw = 1
-    assert_equal 0, @matcher.match('test')
+    assert_in_delta 0, @matcher.match('test'), D
   end
 end
   # vim: set et sw=2 ts=2:
--- amatch/tests/test_pair_distance.rb
+++ amatch/tests/test_pair_distance.rb
@@ -2,36 +2,53 @@ require 'test/unit'
 require 'amatch'
 
 class TC_PairDistance < Test::Unit::TestCase
-  DELTA = 0.000001
+  D = 0.000001
 
   def setup
-    @matcher = Amatch.new('test')
+    @single = Amatch.new('test')
     @empty = Amatch.new('')
     @france = Amatch.new('republic of france')
     @germany = Amatch.new('federal republic of germany')
   end
 
-  def test_countries
-    assert_in_delta 1.0,       @empty.pair_distance(''), DELTA
-    assert_in_delta 0.5555555, @france.pair_distance('france'), DELTA
-    assert_in_delta 0.1052631, @france.pair_distance('germany'), DELTA
-    assert_in_delta 0.4615384, @germany.pair_distance('germany'), DELTA
+  def test_empty
+    assert_in_delta 1,          @empty.pair_distance(''), D
+    assert_in_delta 0,          @empty.pair_distance('not empty'), D
+    assert_in_delta 0,          @single.pair_distance(''), D
   end
 
-  def test_matcher
-    assert_in_delta 0,          @matcher.pair_distance(''), DELTA
-    assert_in_delta 1,          @matcher.pair_distance('test'), DELTA
-    assert_in_delta 0.8571428,  @matcher.pair_distance('testa'), DELTA
-    assert_in_delta 0.8571428,  @matcher.pair_distance('atest'), DELTA
-    assert_in_delta 0.5714285,  @matcher.pair_distance('teast'), DELTA
-    assert_in_delta 0.8,        @matcher.pair_distance('est'), DELTA
-    assert_in_delta 0.8,        @matcher.pair_distance('tes'), DELTA
-    assert_in_delta 0.4,        @matcher.pair_distance('tst'), DELTA
-    assert_in_delta 0.6666666,  @matcher.pair_distance('best'), DELTA
-    assert_in_delta 0.3333333,  @matcher.pair_distance('tost'), DELTA
-    assert_in_delta 0.6666666,  @matcher.pair_distance('tesa'), DELTA
-    assert_in_delta 0.0,        @matcher.pair_distance('taex'), DELTA
-    assert_in_delta 0.5,        @matcher.pair_distance('aaatestbbb'), DELTA
+  def test_countries
+    assert_in_delta 0.5555555,  @france.pair_distance('france'), D
+    assert_in_delta 0.1052631,  @france.pair_distance('germany'), D
+    assert_in_delta 0.4615384,  @germany.pair_distance('germany'), D
+    assert_in_delta 0.16,       @germany.pair_distance('france'), D
+    assert_in_delta 0.6829268,
+      @germany.pair_distance('german democratic republic'), D
+    assert_in_delta 0.72,
+      @france.pair_distance('french republic'), D
+    assert_in_delta 0.4375,
+      @germany.pair_distance('french republic'), D
+    assert_in_delta 0.5294117,
+      @france.pair_distance('german democratic republic'), D
+  end
+
+  def test_single
+    assert_in_delta 0,          @single.pair_distance(''), D
+    assert_in_delta 1,          @single.pair_distance('test'), D
+    assert_in_delta 0.8571428,  @single.pair_distance('testa'), D
+    assert_in_delta 0.8571428,  @single.pair_distance('atest'), D
+    assert_in_delta 0.5714285,  @single.pair_distance('teast'), D
+    assert_in_delta 0.8,        @single.pair_distance('est'), D
+    assert_in_delta 0.8,        @single.pair_distance('tes'), D
+    assert_in_delta 0.4,        @single.pair_distance('tst'), D
+    assert_in_delta 0.6666666,  @single.pair_distance('best'), D
+    assert_in_delta 0.3333333,  @single.pair_distance('tost'), D
+    assert_in_delta 0.6666666,  @single.pair_distance('tesa'), D
+    assert_in_delta 0.0,        @single.pair_distance('taex'), D
+    assert_in_delta 0.5,        @single.pair_distance('aaatestbbb'), D
+    assert_in_delta 0.6,        @single.pair_distance('aaa test bbb'), D
+    assert_in_delta 0.6,        @single.pair_distance('test aaa bbb'), D
+    assert_in_delta 0.6,        @single.pair_distance('bbb aaa test'), D
   end
 end
   # vim: set et sw=2 ts=2:
