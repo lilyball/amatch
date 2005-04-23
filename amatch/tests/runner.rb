d220fd824f342f290dc72f71d3d227cdc72d5ab1
Florian Frank <flori@ping.de>
2005-04-23 17:55:22 +0000

    added testrunner

---
+++ amatch/tests/runner.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'test/unit/ui/console/testrunner'
+require 'test/unit/testsuite'
+$:.unshift File.expand_path(File.dirname($0))
+require 'test_levenshtein'
+require 'test_pair_distance'
+require 'test_hamming'
+
+class TS_AllTests
+  def self.suite
+    suite = Test::Unit::TestSuite.new 'All tests'
+    suite << TC_Levenshtein.suite
+    suite << TC_PairDistance.suite
+    suite << TC_Hamming.suite
+    suite
+  end
+end
+Test::Unit::UI::Console::TestRunner.run(TS_AllTests)
+  # vim: set et sw=2 ts=2:
