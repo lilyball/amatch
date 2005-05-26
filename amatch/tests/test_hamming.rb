0171be3fa86178460bc61eda533119496933652b
Florian Frank <flori@ping.de>
2005-05-26 07:14:53 +0000

    test similar, too

--- amatch/tests/test_hamming.rb
+++ amatch/tests/test_hamming.rb
@@ -14,9 +14,11 @@ class TC_Hamming < Test::Unit::TestCase
   def test_empty
     assert_in_delta 0, @empty.match(''), D
     assert_in_delta 9, @empty.match('not empty'), D
+    assert_in_delta 1, @empty.similar(''), D
+    assert_in_delta 0, @empty.similar('not empty'), D
   end
 
-  def test_small
+  def test_small_match
     assert_in_delta 4, @small.match(''), D
     assert_in_delta 0, @small.match('test'), D
     assert_in_delta 1, @small.match('testa'), D
@@ -30,6 +32,22 @@ class TC_Hamming < Test::Unit::TestCase
     assert_in_delta 1, @small.match('tesa'), D
     assert_in_delta 3, @small.match('taex'), D
     assert_in_delta 9, @small.match('aaatestbbb'), D
+  end
+
+  def test_small_similar
+    assert_in_delta 0.0, @small.similar(''), D
+    assert_in_delta 1.0, @small.similar('test'), D
+    assert_in_delta 0.8, @small.similar('testa'), D
+    assert_in_delta 0.0, @small.similar('atest'), D
+    assert_in_delta 0.4, @small.similar('teast'), D
+    assert_in_delta 0, @small.similar('est'), D
+    assert_in_delta 0.75, @small.similar('tes'), D
+    assert_in_delta 0.25, @small.similar('tst'), D
+    assert_in_delta 0.75, @small.similar('best'), D
+    assert_in_delta 0.75, @small.similar('tost'), D
+    assert_in_delta 0.75, @small.similar('tesa'), D
+    assert_in_delta 0.25, @small.similar('taex'), D
+    assert_in_delta 0.1, @small.similar('aaatestbbb'), D
   end
 end
   # vim: set et sw=2 ts=2:
